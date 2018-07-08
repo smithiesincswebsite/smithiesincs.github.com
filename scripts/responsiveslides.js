5940b819f966b781ee60f099a8dbcadc4ca24756
xriswhat <xtan@smith.edu>
2018-07-08 15:22:22 -0400

    Update

--- scripts/responsiveslides.js
+++ scripts/responsiveslides.js
@@ -16,8 +16,8 @@
       "auto": true,             // Boolean: Animate automatically, true or false
       "speed": 5000,             // Integer: Speed of the transition, in milliseconds
       "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
-      "pager": false,           // Boolean: Show pager, true or false
-      "nav": false,             // Boolean: Show navigation, true or false
+      "pager": true,           // Boolean: Show pager, true or false
+      "nav": true,             // Boolean: Show navigation, true or false
       "random": false,          // Boolean: Randomize the order of the slides, true or false
       "pause": false,           // Boolean: Pause on hover, true or false
       "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
